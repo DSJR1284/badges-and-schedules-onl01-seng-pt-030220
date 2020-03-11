c062f76aaf5c5d5b1f2f1ea0ddb342a5584dae32
beloved-scraper-9319 <dsjr1284@gmail.com>
2020-03-11 03:03:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,23 +7,23 @@ end
   def batch_badge_creator(names)
     attendees = []
     names.each do |speaker|
-      attendees << speaker
+      attendees << badge_maker(speaker)
     end
     return attendees
   end 
   
-  def assign_rooms(speaker room) 
-    room_number = 1
-    return "Hello, #{name}! You'll be assigned to room #{num}!"
-  end 
+  # def assign_rooms(speaker, room) 
+  #   room_number = 1
+  #   return "Hello, #{name}! You'll be assigned to room #{num}!"
+  # end 
   
-  def printer(input)
-    batch_badge_creator(input).each do |badge|
-      puts badge 
-    end 
-    assign_rooms(input).each do |room|
-      puts room 
-    end 
-  end 
+  # def printer(input)
+  #   batch_badge_creator(input).each do |badge|
+  #     puts badge 
+  #   end 
+  #   assign_rooms(input).each do |room|
+  #     puts room 
+  #   end 
+  # end 
     
   
