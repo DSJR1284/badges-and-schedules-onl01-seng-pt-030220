6e876e17b45cb394afc55fb18cfb3c3cc3fb7e36
beloved-scraper-9319 <dsjr1284@gmail.com>
2020-03-11 02:46:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+
+
+def badge_maker(speaker=name)
+  return "Hello, my name is #{name}."
+end
+  
+  def batch_badge_creator(speaker)
+    new_speaker=[]
+    speaker each.do |speaker|
+      new_speaker<< speaker
+        end 
+  
+  def assign_rooms(speaker) 
+    room_number = 1
+    return "Hello, #{name}! You'll be assigned to room #{num}!"
+  end 
+  
+  def printer(input)
+    batch_badge_creator(input).each do |badge|
+      puts badge 
+    end 
+    assign_rooms(input).each do |room|
+      puts room 
+    end 
+  end 
+    
+  
